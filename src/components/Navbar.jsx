a8d6ba402dedca01fd861301760fe9ff8b5e0e20
yuvaraj-LR <yuvarajrama121@gmail.com>
2024-11-03 13:34:19 +0530

    Include Navbar Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,48 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Link, NavLink, Outlet } from 'react-router-dom'
 
 function Navbar() {
+    const isUserLoggedIn = false;
+
     return (
         <div>
-            <nav>
-                <h1>Navbarr...</h1>
-            </nav>
+            <header>
+                <nav className="navbar navbar-expand-lg navbar-bg">
+                    <div className="container-fluid wrapper make_center">
+                        <div className="navbar-brand">
+                            <NavLink to="/" className="brand-title">
+                                <img src="./logo.png" alt="logo" className="home_logo" />
+                            </NavLink>
+                        </div>
 
-            <Outlet />
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item mx-2">
+                                    <NavLink className={ ({isActive}) => isActive ? "current-nav" : null } to="/">Home</NavLink>
+                                </li>
+                                <li className={`nav-item mx-2 ${!isUserLoggedIn ? "hidden" : ""}`}>
+                                    <NavLink className={ ({isActive}) => isActive ? "current-nav" : null } to="/order">Order</NavLink>
+                                </li>
+                                <li className={`nav-item mx-2 ${!isUserLoggedIn ? "hidden" : ""}`}>
+                                    <NavLink className={ ({isActive}) => isActive ? "current-nav" : null } to="/cart">Cart</NavLink>
+                                </li>
+                            </ul>
+                            <form className="d-flex mx-2" role="search">
+                                <input className="form-control me-2" type="search" placeholder="Search for products" aria-label="Search" />
+                            </form>
+
+                            {!isUserLoggedIn ? <Link className="btn btn-primary" to="/signin">Login</Link> : <div className="flex flex_row flex_center gap-1 btn btn-outline-primary"><span><i className="fa-solid fa-arrow-right-from-bracket"></i></span><div>Logout</div></div>}
+                        </div>
+                    </div>
+                </nav>
+            </header>
+
+            <div>
+                <Outlet />
+            </div>
         </div>
     )
 }
